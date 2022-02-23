d4270ae9b7994f4e0032b88bcd2980e497ac5428
Dave Roverts <me@daveroverts.nl>
2022-02-23 22:53:37 +0100

    feat: add sentry
    -

--- pages/index.js
+++ pages/index.js
@@ -49,6 +49,12 @@ export default function Home() {
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
+
+          <button type="button" onClick={() => {
+            throw new Error("Sentry Frontend Error");
+          }}>
+            Throw error
+          </button>
         </div>
       </main>
 
